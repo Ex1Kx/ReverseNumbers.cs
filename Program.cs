34efab9e89a4ad5c9ed1993e3a1cef1f2a2cb147
Ex1Kx <thenewestekigoh@gmail.com>
2022-02-07 17:41:36 -0500

    [Ex1] Added The Exercise

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReverseNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  line, und,dec, ax;
+            Console.Write("Enter U Number: ");
+            line = Convert.ToInt32(Console.ReadLine());
+            dec = line / 10;
+            und = line % 10;
+            ax = (und * 10) + dec;
+            Console.Write("Ur Number Is:" + ax);
+        }
+    }
+}
